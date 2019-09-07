14f115b7e21874cfa4939371e84aca18b758d72b
Filippo Zaffoni <registration.fi.za@outlook.com>
2019-09-07 12:44:46 +0200

    First test for SPM

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "TaggerKit",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "TaggerKit",
+            targets: ["TaggerKit"]),
+    ],
+    targets: [
+        .target(
+            name: "TaggerKit",
+            path: "TaggerKit",
+            swiftLanguageVersions: [.v5])
+    ]
+)
